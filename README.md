63c24f76ec210763545d4175289f365c7f4e4370
Michael Sprecher <sprecher.m@gmail.com>
2013-01-11 16:48:28 +0100

    added generator script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 nsec3-stuff
 ===========
+
+Just some python scripts which I wrote to understand the NSEC3 RR hashing.
---
+++ generate.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import hashlib
+import binascii
+import base64
+import string
+import sys
+
+__std2nsec3 = string.maketrans(
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567',
+    '0123456789abcdefghijklmnopqrstuv'
+)
+
+if len(sys.argv) < 4:
+    sys.exit('Usage:   ' + sys.argv[0] + ' <domain> <salt> <iterations>\nExample: ' + sys.argv[0] + ' foo.bar.example.com c0ffee 5')
+
+domain     = sys.argv[1]
+salt       = sys.argv[2]
+iterations = sys.argv[3]
+
+tmp = domain.split('.')
+toHash = ''
+# all dots get replaced by the length (in byte) of the following string
+for i in tmp:
+    toHash += chr(len(i))
+    toHash += i
+#append a NULL byte following with the salt(as bytes)
+toHash += '\0' + binascii.a2b_hex(salt)
+
+#initial hashround
+hash = hashlib.sha1(toHash).digest() + binascii.a2b_hex(salt)
+#hash it again with number of iterations
+for i in range(int(iterations)):
+    # salt gets appended in each iteration
+    hash = hashlib.sha1(hash).digest() + binascii.a2b_hex(salt)
+print base64.b32encode(hash[:-2]).translate(__std2nsec3, '=')
